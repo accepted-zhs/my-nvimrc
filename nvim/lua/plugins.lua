fc825593d41208487298ca0ae9a39e653b9f7a97
zesty_fox <acceptedzhs@qq.com>
2022-12-28 17:52:31 +0800

    migrate from packer.nvim to lazy.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,91 +1,83 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
+-- Only required if you have packer configured as `lazy`
 
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+vim.g.mapleader = "\\"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
 end
+vim.opt.runtimepath:prepend(lazypath)
 
-vim.cmd [[packadd packer.nvim]]
+return require('lazy').setup({
+    'folke/tokyonight.nvim',
+    'lifepillar/vim-solarized8',
+    'chriskempson/base16-vim',
+    'sainnhe/sonokai',
 
-return require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    { 'nvim-lualine/lualine.nvim', dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true } },
+    "petertriho/nvim-scrollbar",
+    { 'kdheepak/tabline.nvim', dependencies = { { 'nvim-lualine/lualine.nvim', lazy = true }, {'kyazdani42/nvim-web-devicons', lazy = true } } },
 
-    use 'lifepillar/vim-solarized8'
-    use 'chriskempson/base16-vim'
-    use 'folke/tokyonight.nvim'
-    use 'sainnhe/sonokai'
-
-    use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-    use 'nvim-tree/nvim-web-devicons'
-    use {"petertriho/nvim-scrollbar" }
-    use {
-        'kdheepak/tabline.nvim',
-        requires = { { 'hoob3rt/lualine.nvim', opt=true }, {'kyazdani42/nvim-web-devicons', opt = true} }
-    }
-
-    use {
+    {
         'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
-    use {
+        config = function() require('Comment').setup() end
+    },
+    {
     	"windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
-    }
+    },
 
-    use {
+    {
         'goolord/alpha-nvim',
-        requires = { 'kyazdani42/nvim-web-devicons' },
+        dependencies = { 'kyazdani42/nvim-web-devicons' },
         config = function ()
             require'alpha'.setup(require'alpha.themes.startify'.config)
         end
-    }
-    use 'ryanoasis/vim-devicons'
-    use "lukas-reineke/indent-blankline.nvim"
-    use {
+    },
+    'ryanoasis/vim-devicons',
+    "lukas-reineke/indent-blankline.nvim",
+    {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
-    }
+    },
 
-    use 'simnalamburt/vim-mundo'
-    use { 'CRAG666/code_runner.nvim', requires = 'nvim-lua/plenary.nvim' }
-    use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
+    'simnalamburt/vim-mundo',
+    { 'CRAG666/code_runner.nvim', dependencies = 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope.nvim', dependencies = { {'nvim-lua/plenary.nvim'} } },
 
-    use {
+    {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v2.x",
-        requires = {
+        dependencies = {
             "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
         },
-    }
-    use 'tpope/vim-fugitive'
+    },
+    'tpope/vim-fugitive',
 
-    use { -- LSP
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-        'hrsh7th/nvim-cmp', -- Autocompletion plugin
-        'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
-        'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/cmp-buffer',
-        'L3MON4D3/LuaSnip', -- Snippets plugin
-        "ray-x/lsp_signature.nvim",
-        { "glepnir/lspsaga.nvim", branch = "main" }, -- LSP UI
-
-        'p00f/clangd_extensions.nvim', -- Clangd extensions
-    }
-
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+    -- LSP
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    'hrsh7th/nvim-cmp', -- Autocompletion plugin
+    'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+    'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-buffer',
+    'L3MON4D3/LuaSnip', -- Snippets plugin
+    "ray-x/lsp_signature.nvim",
+    { "glepnir/lspsaga.nvim", branch = "main" }, -- LSP UI
+    'p00f/clangd_extensions.nvim', -- Clangd extensions
+})
 
