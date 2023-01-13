ff61a8bd70bf68556ef1a969118f6cd81a4bc942
Zesty_Fox <acceptedzhs@qq.com>
2023-01-14 07:49:54 +0800

    add completions for neovim runtime

--- nvim/lua/plugin-config/lsp.lua
+++ nvim/lua/plugin-config/lsp.lua
@@ -42,8 +42,31 @@ require("mason-lspconfig").setup_handlers {
     end,
 }
 
--- language extensions
+-- clangd
 require("clangd_extensions").setup()
+-- sumneko_lua
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 -- Completions
 
