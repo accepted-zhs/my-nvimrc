370e0c8ce536e9ea2b1854b020d46780f58052fe
Zesty_Fox <acceptedzhs@qq.com>
2023-02-02 17:09:29 +0800

    revert runner to asyncrun.vim

--- nvim/lua/plugin-config/runner.lua
+++ nvim/lua/plugin-config/runner.lua
@@ -1,43 +1,25 @@
-require('code_runner').setup({
-  -- put here the commands by filetype
-  filetype = {
-    java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-    python = "python3 -u \"$fileName\"",
-    typescript = "deno run",
-    rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
-    cpp = "cd \"$dir\" && g++ \"$fileName\" -o \"$fileNameWithoutExt\" -Wall -Wextra -fsanitize=address,undefined -g3 -DLOCAL"
-  },
-  -- choose default mode (valid term, tab, float, toggle, buf)
-  mode = 'term',
-  -- Focus on runner window(only works on toggle, term and tab mode)
-  focus = false,
-  -- startinsert (see ':h inserting-ex')
-  startinsert = false,
-  float = {
-    -- Key that close the code_runner floating window
-    close_key = '<ESC>',
-    -- Window border (see ':h nvim_open_win')
-    border = "rounded",
+vim.cmd([[
+func! CompileFile()
+    let opts = {"cwd": $VIM_FILEPATH, "mode": "term", "rows": 10, "focus": 0}
+    if &filetype == 'cpp'
+        call asyncrun#run("!", opts, "g++ \"$VIM_FILEPATH\" -o \"$VIM_PATHNOEXT\" -DLOCAL -Wall -Wextra -std=c++14 -g3 -fsanitize=address,undefined")
+    elseif &filetype == 'c'
+        call asyncrun#run("!", opts, "gcc \"$VIM_FILEPATH\" -o \"$VIM_PATHNOEXT\" -Wall -Wextra")
+    endif
+endfunc
 
-    -- Num from `0 - 1` for measurements
-    height = 0.6,
-    width = 0.8,
-    x = 0.5,
-    y = 0.5,
+func! RunFile()
+    call asyncrun#stop("")
+    let opts = {"cwd": $VIM_FILEPATH, "mode": "term", "rows": 10}
+    if &filetype == 'cpp' || &filetype == 'c'
+        call asyncrun#run("!", opts, "/usr/bin/time -f \"user time: %U s\nreal time: %e s\nmemory: %M kb\" \"$VIM_PATHNOEXT\"")
+    elseif &filetype == 'python'
+        call asyncrun#run("!", opts, "python3 -u \"$VIM_FILEPATH\"")
+    elseif &filetype == 'sh'
+        call asyncrun#run("!", opts, "bash \"$VIM_FILEPATH\"")
+    endif
+endfunc
+]])
 
-    -- Highlight group for floating window/border (see ':h winhl')
-    border_hl = "FloatBorder",
-    float_hl = "Normal",
-
-    -- Transparency (see ':h winblend')
-    blend = 30,
-  },
-})
-
--- vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<F5>', ':RunFile<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<F5>', ':call CompileFile()<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<F6>', ':call RunFile()<CR>', { noremap = true, silent = false })
