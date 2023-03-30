ef8ddc2f938927b31c9de2220f00db4a8914dd14
Zesty_Fox <acceptedzhs@qq.com>
2023-03-30 14:15:11 +0800

    lualine: add datetime component

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,7 +13,28 @@ return {
             require("lualine").setup {
                 options = {
                     theme = 'tokyonight'
-                }
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {
+                        'location', {
+                            'datetime',
+                            style = "%Y/%m/%d %H:%M"
+                        }
+                    }
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {'filename'},
+                    lualine_x = {'location'},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
             }
         end
     },
