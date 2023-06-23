b7defdc49cf4731a362a5e3c6f9470299d79e1f6
Zesty_Fox <acceptedzhs@qq.com>
2023-06-23 22:52:54 +0800

    coding: add motion plugin

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -78,5 +78,14 @@ return {
                 }
             })
         end
+    },
+    {
+        'ggandor/leap.nvim',
+        dependencies = {
+            'tpope/vim-repeat'
+        },
+        config = function ()
+            require('leap').add_default_mappings()
+        end
     }
 }
